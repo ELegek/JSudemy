6f28197f97378b2482ac579a0bc969d854ee53c9
Evgeny <evgeny.legek@yandex.ru>
2020-08-25 17:25:51 +0300

    Выполнил первое задание

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,26 @@
 'use strict'
 
+let money = +prompt("Ваш бюджет на месяц?", '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+
+
+let appData = {
+    budjet: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+}
+
+let answer1 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let answer2 = +prompt('Во сколько обойдется?', '');
+let answer3 = prompt('Введите обязательную статью расходов в этом месяце', '');
+let answer4 = +prompt('Во сколько обойдется?', '');
+
+appData.expenses.answer1 = answer2;
+appData.expenses.answer3 = answer4;
+
+
+console.log(appData.budjet/30);
