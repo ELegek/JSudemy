cb74abf8cff8ded2b3d41d0bd36b708e8e4acef0
Evgeny <evgeny.legek@yandex.ru>
2020-08-26 20:37:21 +0300

    Выполнил 2 задание

--- app/js/main.js
+++ app/js/main.js
@@ -28,6 +28,43 @@ for (let i = 0; i < 2; i++) {
 
 }
 
+// Цикл WHILE
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = +prompt('Во сколько обойдется?', '');
+//
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a !== '' && b !== '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log('Ошибка');
+//         i--;
+//     }
+    i++
+// }
+
+// Цикл Do While
+// let i = 0;
+// do {
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = +prompt('Во сколько обойдется?', '');
+//
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a !== '' && b !== '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log('Ошибка');
+//         i--;
+//     }
+//     i++
+// } while (i < 2);
+
+
+
+
+
+
 appData.moneyPerDay = appData.budget/30;
 
 console.log('Ежедневный бюджет: ' + appData.moneyPerDay);
@@ -41,4 +78,7 @@ if (appData.moneyPerDay < 100) {
 }   else {
     console.log('Ошибка');
 }
+
+
+
 
